07babcb6e48437c7598eef642ee823a3d9113a98
Boaz Cogan <30667841+boazcogan@users.noreply.github.com>
2017-12-04 13:42:37 -0800

    Note addition

--- SSU_directory.py
+++ SSU_directory.py
@@ -2,6 +2,7 @@ import ProfessorParser
 
 
 def main():
+    # I recommend not exceeding 10 threads, 6 is the number of threads chosen after optimization testing.
     a = ProfessorParser.ProfessorParser(6)
     a.spawnThreads()
     a.timeIt()
